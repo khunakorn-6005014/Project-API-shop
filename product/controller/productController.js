fb00a2187825c20c8eefb8db52c9e3212a3e4af6
khuna <khunakorn.w@gmail.com>
2025-06-02 19:46:11 -0700

    add viewByid and get all Product  product run property

--- product/controller/productController.js
+++ product/controller/productController.js
@@ -85,3 +85,66 @@ export const deletedProduct = asyncHandler(async (req, res) => {
         res.status(500).json({ success: false, message: error.message });
     }
 });
+export const viewProductId = asyncHandler(async (req, res) => {
+  try{
+    const { id } = req.params;
+    const userId = req.userData?.userId || "Guest"; // ✅ Allows guests to view
+    console.log("User Data in view:", userId);
+     const product = await Product.findOne({ productId: id });
+    if (!product) {
+            return res.status(404).json({ message: "Product not found." });
+    }
+
+  res.status(200).json({ success: true, message: "Product details retrieved.", product });
+  }catch (error) {
+     res.status(500).json({ success: false, message: error.message });
+   }
+});
+export const getAllProduct = asyncHandler(async (req, res) => {
+  let queryObj = { ...req.query };
+    ['page', 'sort', 'limit', 'fields']
+        .forEach(el => delete queryObj[el]);  // phase 2 - advance filtering
+    const strQuery = JSON.stringify(queryObj)
+        .replace(/\b(gte|gt|lte|lt)\b/g,
+            match =>`$${match}`);
+    queryObj = JSON.parse(strQuery);
+    console.log("Received Query Params:", req.query);
+    console.log("Processed Query Object:",queryObj);
+    
+    const sort = req.query.sort ?req.query.sort.split(',').join(' ') : "";
+    
+    const selected = req.query.fields ? req.query.fields.split(',').join(' ') : "";
+    const limit = Number(req.query.limit) || 100;
+    const page = Number(req.query.page) || 1;
+    const skip = (page - 1) * limit;
+    console.log("Sorting By:", sort);
+   console.log("Selected Fields:", selected);
+//http://localhost:3000/product/Products?page=1&sort=email
+    try{
+    const userId = req.userData?.userId || "Guest"; // ✅ Allows guests to view
+    console.log("User Data in view:", userId);
+        const documents = await Product.countDocuments(queryObj);
+    if (documents === 0) {
+    return res.status(200).json({ success: true, data: [], message: "No products found." });
+}
+        if (skip >= documents && documents > 0) {
+    return res.status(200).json({
+         success: true, data: [], message: "No more products to show on this page." });
+      } 
+      const product = await Product.find(queryObj).skip(skip)
+       .limit(limit)
+       .select(selected)
+       .sort(sort);
+       return res.status(200).json({
+           data: product,
+           success: true, 
+           page,
+           limit,
+           totalPages: Math.ceil(documents / limit),
+           documents,
+           message: "product list" });
+  }
+  catch (error) {
+     res.status(500).json({ success: false, message: error.message });
+   }
+});
