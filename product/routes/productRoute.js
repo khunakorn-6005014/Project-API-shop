bf8526cc0d3239a4453c45c3f82f187b847392a6
khuna <khunakorn.w@gmail.com>
2025-06-02 19:46:11 -0700

    add viewByid and get all Product  product run property

--- product/routes/productRoute.js
+++ product/routes/productRoute.js
@@ -1,10 +1,12 @@
 //APIproject/product/routes/productRoutes.js
 import express from "express"
 import verifyToken from "../../config/auth.middleware.js"
-import { createProduct ,updateProduct,deletedProduct} from "../controller/productController.js";
+import { createProduct ,updateProduct,deletedProduct,viewProductId,getAllProduct} from "../controller/productController.js";
 
 const router = express.Router();
 router.post("/new-product", verifyToken,createProduct)
 router.post("/updated/:id", verifyToken,updateProduct)
 router.delete("/deleted/:id", verifyToken,deletedProduct)
+router.get("/deteil/:id", viewProductId)
+router.get("/Products", getAllProduct)
 export default router;
