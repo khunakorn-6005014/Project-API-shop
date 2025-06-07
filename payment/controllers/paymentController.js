5157a8a9ff5a3a951554cd841f481b51c1ae59cb
khuna <khunakorn.w@gmail.com>
2025-06-07 02:41:56 -0700

    update payment

--- payment/controllers/paymentController.js
+++ payment/controllers/paymentController.js
@@ -3,19 +3,50 @@ import PaymentService from "../services/paymentService.js";
 
 // Endpoint to process a payment for an order
 export const processPayment = asyncHandler(async (req, res) => {
-  const { orderId, amount, paymentMethod } = req.body;
+ try{
   const  userId  = req.userData.userId; // ensure your verifyToken middleware attaches userData
   console.log("User Data in payment:", userId)
+  const {orderId , amount, paymentMethod}= req.body;
+  
   const payment = await PaymentService.processPayment({
     orderId,
     userId,
     amount,
     paymentMethod,
   });
-  
-  res.status(200).json({
-    success: true,
-    message: "Payment processed successfully",
-    payment,
-  });
+  res.status(200).json({success: true, message: "Payment processed successfully",payment});
+ }catch(error){
+   console.error("Payment processing error:", error.message); // Log the error
+    await Payment.create({
+        paymentId: uuidv4(),
+        orderId,
+        userId,
+        amount,
+        paymentMethod,
+        status: "failed",   
+      });// Optionally, capture the error message/details
+      await Order.findOneAndUpdate({ orderId }, { status: "payment_failed" });
+          // Send an appropriate error response without throwing another error
+    return res.status(500).json({
+        success: false, 
+        message: "Payment processing failed",
+        error: error.message
+    });
+  }
 });
+export const refundPayment = asyncHandler(async (req, res) => {
+ try{
+  const  userId  = req.userData.userId; // ensure your verifyToken middleware attaches userData
+  console.log("User Data in payment:", userId);
+  const {orderId , refundAmount}= req.body;
+  const payment = await PaymentService.refundPayment({
+    orderId,
+    userId,
+    refundAmount,
+     });
+  res.status(200).json({success: true,message: "Payment refunded successfully",payment});
+ }
+ catch(error){
+  res.status(500).json({ success: false, message: error.message });
+ }
+    });
