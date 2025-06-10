c6f722cc380f02ff37d8371d63136ed6470fed65
khuna <khunakorn.w@gmail.com>
2025-06-10 01:39:20 -0700

    update  docker and kubernete  for payment and shipping and notifiaction

--- notification/server.js
+++ notification/server.js
@@ -2,9 +2,10 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
-import notificationRoutes from "./controllers/notificationController.js"; // Adjust path if needed
+import { getUserNotifications, markNotificationAsRead } from "./controllers/notificationController.js"; // Adjust path if needed
 import dotenv from 'dotenv';
 dotenv.config();
+const app = express(); 
 
 const PORT = process.env.PORT || 3001;
 const DB_URL = process.env.MONGODB_URL;
@@ -12,10 +13,12 @@ const DB_URL = process.env.MONGODB_URL;
 mongoose.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Connected to MongoDB"))
   .catch(err => console.error("MongoDB Connection Error:", err));
+console.log("MongoDB URL from .env:", process.env.MONGODB_URL);
 
 app.use(express.json());
 app.use(cors());
-app.use("/notifications", notificationRoutes);
+app.use("/notifications", getUserNotifications);
+app.use("/notifications/read", markNotificationAsRead);
 
 app.listen(PORT, () =>
   console.log(`Notifications Service running on port ${PORT}`)
