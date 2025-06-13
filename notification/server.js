3b8fc5d74740236ed88929e6244e521574a7d365
khuna <khunakorn.w@gmail.com>
2025-06-13 01:35:22 -0700

    update  docker and kubernete  for notification found error in node-module

--- notification/server.js
+++ notification/server.js
@@ -2,24 +2,34 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
-import { getUserNotifications, markNotificationAsRead } from "./controllers/notificationController.js"; // Adjust path if needed
+import notificationRoutes from "./routes/notificationRoutes.js";
 import dotenv from 'dotenv';
-dotenv.config();
-const app = express(); 
+import { initConsumer,shutdownConsumer } from "./mq/consumer.js";
 
-const PORT = process.env.PORT || 3001;
-const DB_URL = process.env.MONGO_URI;
-console.log("MongoDB URI from .env:", DB_URL);
+
+dotenv.config();
+async function start() {
+ const app = express(); 
+ const PORT = process.env.PORT;
+ const DB_URL = process.env.MONGO_URI;
+ console.log("MongoDB URI from .env:", DB_URL);
 mongoose.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Connected to MongoDB"))
   .catch(err => console.error("MongoDB Connection Error:", err));
-
+initConsumer();
 
 app.use(express.json());
 app.use(cors());
-app.use("/notifications", getUserNotifications);
-app.use("/notifications/read", markNotificationAsRead);
+app.use("/notifications", notificationRoutes);
 
 app.listen(PORT, () =>
   console.log(`Notifications Service running on port ${PORT}`)
 );
+process.on('SIGINT',  async () => { await shutdownConsumer(); process.exit(); });
+process.on('SIGTERM', async () => { await shutdownConsumer(); process.exit(); });
+
+}
+start().catch(err => {
+  console.error('Fatal startup error', err);
+  process.exit(1);
+});
